b6678163486298b56068818db976b246199236e4
ElvineJoy <elvinkuiyungest@gmail.com>
2024-06-04 15:52:53 +0300

    forms in react understood

--- src/components/App.js
+++ src/components/App.js
@@ -9,12 +9,16 @@ function App() {
 
   function handleDarkModeClick() {
     setIsDarkMode((isDarkMode) => !isDarkMode);
-  }
+    }
+
+  function handleNewItemSubmit(NewItem){
+    setItems([...items, NewItem]);
+    }
 
   return (
     <div className={"App " + (isDarkMode ? "dark" : "light")}>
       <Header isDarkMode={isDarkMode} onDarkModeClick={handleDarkModeClick} />
-      <ShoppingList items={items} />
+      <ShoppingList items={items} onItemFormSubmit={handleNewItemSubmit} />
     </div>
   );
 }
